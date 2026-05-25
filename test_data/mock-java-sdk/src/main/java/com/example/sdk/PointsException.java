a814347bd0c620e0987a778e156889b56acfb08b
tppioneer <2606872514@qq.com>
2026-05-25 22:51:05 +0800

    删除测试数据

--- test_data/mock-java-sdk/src/main/java/com/example/sdk/PointsException.java
+++
@@ -1,7 +0,0 @@
-package com.example.sdk;
-
-public class PointsException extends RuntimeException {
-    public PointsException(String message) {
-        super(message);
-    }
-}
--- test_data/revision-sdk/src/main/java/com/example/RevisionUtil.java
+++
@@ -1,6 +0,0 @@
-package com.example;
-
-public class RevisionUtil {
-    public void doWork(String input) {
-    }
-}
